59678b5ec658d13ed82bbda7f2d64d6554c802c1
Shekhar Shukla <nk.shekhar24@gmail.com>
2020-09-08 13:15:22 +0530

    separate c++ and r extension modules

--- src/pyOpenMS/create_r_extension.py
+++ src/pyOpenMS/create_r_extension.py
@@ -7,14 +7,14 @@ iswin = sys.platform == "win32"
 
 # make sure we only log errors and not info/debug ...
 from logging import basicConfig
-# from logging import CRITICAL, ERROR, WARNING, INFO, DEBUG
+from logging import CRITICAL, ERROR, WARNING, INFO, DEBUG
 basicConfig(level=21)
 
-# import config
+import config
 from env import (QT_QMAKE_VERSION_INFO, OPEN_MS_BUILD_TYPE, OPEN_MS_SRC,
                  OPEN_MS_CONTRIB_BUILD_DIRS, OPEN_MS_LIB, OPEN_SWATH_ALGO_LIB, SUPERHIRN_LIB,
                  OPEN_MS_BUILD_DIR, MSVS_RTLIBS, OPEN_MS_VERSION,
-                 Boost_MAJOR_VERSION, Boost_MINOR_VERSION, PY_NUM_THREADS, PY_NUM_MODULES)
+                 Boost_MAJOR_VERSION, Boost_MINOR_VERSION)
 
 IS_DEBUG = OPEN_MS_BUILD_TYPE.upper() == "DEBUG"
 
@@ -32,7 +32,7 @@ import os
 import shutil
 
 classdocu_base = "http://www.openms.de/current_doxygen/html/"
-autowrap.CodeGenerator.special_class_doc = "\n    Documentation is available at " + classdocu_base + "class%(namespace)s_1_1%(cpp_name)s.html\n"
+autowrap.CodeGenerator.special_class_doc = "Documentation is available at " + classdocu_base + "class%(namespace)s_1_1%(cpp_name)s.html\n"
 autowrap.DeclResolver.default_namespace = "OpenMS"
 
 def chunkIt(seq, num):
@@ -49,10 +49,14 @@ def chunkIt(seq, num):
 
 j = os.path.join
 
+# Threads and Modules
+PY_NUM_THREADS = 1
+PY_NUM_MODULES = 1
+
 src_pyopenms = j(OPEN_MS_SRC, "src/pyOpenMS")
 pxd_files = glob.glob(src_pyopenms + "/pxds/*.pxd")
-addons = glob.glob(src_pyopenms + "/addons/*.pyx")
-converters = [j(src_pyopenms, "converters")]
+addons = glob.glob(src_pyopenms + "/R-addons/*.R")
+converters = [j(src_pyopenms, "R-converters")]
 
 persisted_data_path = "include_dir.bin"
 
@@ -69,29 +73,11 @@ for de in decls:
     tmp.append(de)
     pxd_decl_mapping[ de.cpp_decl.pxd_path] = tmp
 
-# add __str__ if toString() method is declared:
-for d in decls:
-    # enums, free functions, .. do not have a methods attribute
-    methods = getattr(d, "methods", dict())
-    to_strings = []
-    for name, mdecls in methods.items():
-        for mdecl in mdecls:
-            name = mdecl.cpp_decl.annotations.get("wrap-cast", name)
-            name = mdecl.cpp_decl.annotations.get("wrap-as", name)
-            if name == "toString":
-                to_strings.append(mdecl)
-
-    for to_string in to_strings:
-        if len(to_string.arguments) == 0:
-            d.methods.setdefault("__str__", []).append(to_string)
-            print("ADDED __str__ method to", d.name)
-            break
 
 # Split into chunks based on pxd files and store the mapping to decls, addons
 # and actual pxd files in a hash. We need to produce the exact number of chunks
 # as setup.py relies on it as well.
 pxd_files_chunk = chunkIt(list(pxd_decl_mapping.keys()), int(PY_NUM_MODULES))
-print (len(pxd_files_chunk), PY_NUM_MODULES)
 
 # Sanity checks: we should find all of our chunks and not have lost files
 if len(pxd_files_chunk) != int(PY_NUM_MODULES):
@@ -108,28 +94,12 @@ for pxd_f, m in zip(pxd_files_chunk, mnames):
 
     allDecl_mapping[m] =  {"decls" : tmp_decls, "addons" : [] , "files" : pxd_f}
 
-# Deal with addons, make sure the addons are added to the correct compilation
-# unit (e.g. where the name corresponds to the pxd file).
-# Note that there are some special cases, e.g. addons that go into the first
-# unit or all *but* the first unit.
+
+# Deal with addons, make sure the addons are added to the correct class generator.
 is_added = [False for k in addons]
 for modname in mnames:
 
     for k,a in enumerate(addons):
-        # Deal with special code that needs to go into all modules, only the
-        # first or only all other modules...
-        if modname == mnames[0]:
-            if os.path.basename(a) == "ADD_TO_FIRST" + ".pyx":
-                allDecl_mapping[modname]["addons"].append(a)
-                is_added[k] = True
-        else:
-            if os.path.basename(a) == "ADD_TO_ALL_OTHER" + ".pyx":
-                allDecl_mapping[modname]["addons"].append(a)
-                is_added[k] = True
-        if os.path.basename(a) == "ADD_TO_ALL" + ".pyx":
-            allDecl_mapping[modname]["addons"].append(a)
-            is_added[k] = True
-
         # Match addon basename to pxd basename
         for pfile in allDecl_mapping[modname]["files"]:
             if os.path.basename(a).split(".")[0] == os.path.basename(pfile).split(".")[0]:
@@ -142,7 +112,7 @@ for modname in mnames:
         # Also match by class name (sometimes one pxd contains multiple classes
         # and the addon is named after one of them)
         for dclass in allDecl_mapping[modname]["decls"]:
-            if os.path.basename(a) == dclass.name + ".pyx":
+            if os.path.basename(a) == dclass.name + ".R":
                 allDecl_mapping[modname]["addons"].append(a)
                 is_added[k] = True
 
@@ -153,68 +123,42 @@ for k, got_added in enumerate(is_added):
         for m in mnames:
             allDecl_mapping[m]["addons"].append( addons[k] )
 
+def doRCodeGeneration(modname, allDecl_mapping, instance_map, converters):
+    # create the ropenms/ and R/ directory.
+    src_code_dir = j(OPEN_MS_SRC,"src/ropenms/R")
 
-def doCythonCodeGeneration(modname, allDecl_mapping, instance_map, converters):
-    m_filename = "pyopenms/%s.pyx" % modname
+    try:
+        os.makedirs(src_code_dir)
+    except OSError:
+        pass
+    
+    m_filename = j(src_code_dir,"%s.R" % modname)
     cimports, manual_code = autowrap.Main.collect_manual_code(allDecl_mapping[modname]["addons"])
     autowrap.Main.register_converters(converters)
+    # Need to specify binding "R" as default is "cython"
     autowrap_include_dirs = autowrap.generate_code(allDecl_mapping[modname]["decls"], instance_map,
                                                         target=m_filename, debug=False, manual_code=manual_code,
                                                         extra_cimports=cimports,
-                                                        include_boost=False, include_numpy=True, allDecl=allDecl_mapping)
+                                                        include_boost=False, include_numpy=True, allDecl=allDecl_mapping, target_binding="R")
     allDecl_mapping[modname]["inc_dirs"] = autowrap_include_dirs
+
+    # 
+    # copy the tests folder from ropenms-attachments to ropenms
+    #
+    dest = j(OPEN_MS_SRC,"src/ropenms/tests")
+
+    if os.path.isdir(dest):
+        shutil.rmtree(dest, ignore_errors=True)
+    
+    src = j(OPEN_MS_SRC,"src/pyOpenMS/ropenms-attachments/tests")
+    shutil.copytree(src,dest)
     return autowrap_include_dirs
 
-def doCythonCompile(arg):
-    """
-    Perform the Cython compilation step for each module
-    """
-
-    modname, autowrap_include_dirs = arg
-    m_filename = "pyopenms/%s.pyx" % modname
-    print ("Cython compile", m_filename)
-    autowrap.Main.run_cython(inc_dirs=autowrap_include_dirs, extra_opts={"compiler_directives": {"language_level": 2}}, out=m_filename)
-
-    if False:
-        #
-        # Fix two bugs in the cpp code generated by Cython to allow error-free
-        # compilation (see OpenMS issues on github #527 and #745).
-        #
-        import re
-        f = open(m_filename)
-        fout = open(m_filename + "tmp", "w")
-        expr_fix = re.compile(r"(.*).std::vector<(.*)>::iterator::~iterator\(\)")
-        for line in f:
-            # Fix for Issue #527
-            res = expr_fix.sub('typedef std::vector<\\2>::iterator _it;\n\\1.~_it()', line)
-            # Fix for Issue #745
-            res = res.replace("__Pyx_PyUnicode_FromString(char", "__Pyx_PyUnicode_FromString(const char")
-            fout.write(res)
-
-        fout.close()
-        f.close()
-        shutil.copy(m_filename + "tmp", m_filename)
-        os.remove(m_filename + "tmp")
 
 for modname in mnames:
-    autowrap_include_dirs = doCythonCodeGeneration(modname, allDecl_mapping, instance_map, converters)
+    autowrap_include_dirs = doRCodeGeneration(modname, allDecl_mapping, instance_map, converters)
     pickle.dump(autowrap_include_dirs, open(persisted_data_path, "wb"))
 
 argzip = [ (modname, allDecl_mapping[modname]["inc_dirs"]) for modname in mnames]
-for arg in argzip:
-    doCythonCompile(arg)
 
-print("created pyopenms.cpp")
-
-
-with open("pyopenms/all_modules.py", "w") as fp:
-    for modname in mnames:
-        fp.write("from .%s import *\n" % modname)
-
-
-# create version information
-version = OPEN_MS_VERSION
-
-print("version=%r\n" % version, file=open("pyopenms/version.py", "w"))
-print("info=%r\n" % QT_QMAKE_VERSION_INFO, file=open("pyopenms/qt_version_info.py", "w"))
-
+print("created ropenms package")
